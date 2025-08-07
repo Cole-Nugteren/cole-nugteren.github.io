51467866907fae5827e8cddf8c8b0f8ca5bb3499
Cole Nugteren <canslp01@gmail.com>
2025-08-07 18:42:53 -0500

    generate works

---
+++ docs/works.js
@@ -0,0 +1,49 @@
+class Thumbnail {
+    constructor(id, title, image) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+    }
+    generate() {
+        var work = document.createElement("div");
+        work.id = "work-thumbnail";
+        var image = document.createElement("img");
+        image.src = "assets/works thumbnails/" + image;
+        image.width = "315";
+        //image.height = "250";
+        image.style = "position:relative;";
+        work.appendChild(image);
+        var subtitle = document.createElement("a");
+        subtitle.id = "darklink";
+        subtitle.innerHTML = title;
+        work.appendChild(subtitle);
+        center.appendChild(work);
+    }
+}
+
+const center = document.getElementById("center-content")
+
+const game_works = [
+    new Thumbnail("tubular", "TUBULAR!!", "tubular.png"),
+    new Thumbnail("ripple-and-frawg", "Ripple and Frawg: The Seasonal Song", "ripple-and-frawg.png")
+];
+
+for (const thumb of game_works) {
+    thumb.generate();
+}
+
+/*for (var i = 0; i < 18; i++) {
+    var work = document.createElement("div")
+    work.id = "work-thumbnail"
+    var image = document.createElement("img")
+    image.src = "assets/works thumbnails/turtle-herdle.png"
+    image.width = "315"
+    image.height = "250"
+    image.style = "position:relative;"
+    work.appendChild(image)
+    var subtitle = document.createElement("a")
+    subtitle.id = "darklink"
+    subtitle.innerHTML = "turtle herdle"
+    work.appendChild(subtitle)
+    center.appendChild(work)
+}*/
